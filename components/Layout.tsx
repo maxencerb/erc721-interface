6917e937df847acfc9ae61ed4ef0b80502727642
maxencerb <maxenceraballand00@gmail.com>
2022-03-15 18:34:28 +0100

    Adding route guard for chain id

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
-import { Container } from '@chakra-ui/react'
+import { Alert, AlertDescription, AlertIcon, AlertTitle, Button, Center, Container } from '@chakra-ui/react'
+import { Rinkeby, useEthers } from '@usedapp/core'
 import React from 'react'
 import Topbar from './Topbar'
 
@@ -6,7 +7,51 @@ type Props = {
     children: React.ReactNode
 }
 
+declare global {
+  interface Window {
+      ethereum: any;
+  }
+}
+
+function DefaultChildren() {
+
+  const switchNetwork = async () => {
+    await window.ethereum.request({
+      method: 'wallet_switchEthereumChain',
+      params: [{ chainId: '0x04' }],
+    });
+  }
+
+  return (
+    <Center>
+      <Alert
+        status='error'
+        variant='subtle'
+        flexDirection='column'
+        alignItems='center'
+        justifyContent='center'
+        textAlign='center'
+        height='300px'
+      >
+        <AlertIcon boxSize='40px' mr={0} />
+        <AlertTitle mt={4} mb={1} fontSize='lg'>
+          Connect to rinkeby on metamask
+        </AlertTitle>
+        <AlertDescription maxWidth='md'>
+          First connect to metamask then choose the rinkeby network !
+        </AlertDescription>
+        <Button m={4} p={2} onClick={switchNetwork}>
+          Connect to rinkeby
+        </Button>
+      </Alert>
+    </Center>
+  )
+}
+
 export default function Layout({ children }: Props) {
+
+  const { account, chainId } = useEthers();
+
   return (
     <>
       <Topbar />
@@ -14,7 +59,7 @@ export default function Layout({ children }: Props) {
         p='4'
         maxW='1200px'
       >
-        {children}
+        {account && chainId === Rinkeby.chainId ? children : <DefaultChildren />}
       </Container>
     </>
   )
