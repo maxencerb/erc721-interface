04408bfa212a796e5183720376419d2f3466c6dc
maxencerb <maxenceraballand00@gmail.com>
2022-03-15 17:42:44 +0100

    fix identicon

--- components/Identicon.tsx
+++ components/Identicon.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { useEthers } from "@usedapp/core";
-import styled from "@emotion/styled";
-import Jazzicon from "@metamask/jazzicon";
+import JazzIcon, { jsNumberForAddress } from 'react-jazzicon'
 
 type Props = {
   diameter?: number;
@@ -9,27 +8,11 @@ type Props = {
 }
 
 export default function Identicon({ diameter = 16, address }: Props) {
-  const ref = useRef<HTMLDivElement>();
+
   const { account } = useEthers();
 
-  const finalAdress = address || account;
+  const finalAdress = address || account || "";
 
-  console.log(finalAdress);
-
-  useEffect(() => {
-    if (finalAdress && ref.current) {
-      ref.current.innerHTML = "";
-      ref.current.appendChild(Jazzicon(diameter, parseInt(finalAdress.slice(2, 10), 16)));
-    }
-  }, [finalAdress, diameter]);
-
-  return <div 
-    ref={ref as any} 
-    style={{
-      height: diameter,
-      width: diameter,
-      borderRadius: "50%",
-      backgroundColor: "black",
-    }}
-  />
+  return <JazzIcon diameter={diameter} seed={jsNumberForAddress(finalAdress)} />;
+  
 }
